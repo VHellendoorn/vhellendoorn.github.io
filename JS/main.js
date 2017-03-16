c6e06809f26dd864196e5afb8644de5d58272fef
vhellendoorn <vhellendoorn@live.nl>
2017-03-16 10:01:34 -0700

    Moved sidebar, small text changes

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,9 @@
+$("#menu-toggle").click(function(e) {
+	e.preventDefault();
+	$("#wrapper").toggleClass("toggled");
+});
+$("#sidebar-wrapper").load("sidebar.html");
+
 function load(page, element) {
 	$("#sidebar-wrapper .selected-link").each(function(k, v) { $(v).toggleClass("selected-link") });
 	$(element).parent().toggleClass("selected-link");

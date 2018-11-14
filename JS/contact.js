53be1e9db5467db2e220db1375ba9f08c553737e
vhellendoorn <vhellendoorn@live.nl>
2018-11-13 16:20:38 -0800

    Update to loc on maps

--- JS/contact.js
+++ JS/contact.js
@@ -1,11 +1,10 @@
 function initMap() {
-var loc = {lat: 38.535321, lng: -121.7543463};
 var map = new google.maps.Map(document.getElementById('map-container'), {
-  zoom: 9,
-  center: loc
+  zoom: 12,
+  center: {lat: 38.551520, lng: -121.739919}
 });
 var marker = new google.maps.Marker({
-  position: loc,
+  position: {lat: 38.535322, lng: -121.752697},
   map: map
 });
 }

c54e220230a63ed76604cdb81125d1258cc5ec8b
vhellendoorn <vhellendoorn@live.nl>
2018-11-13 16:13:51 -0800

    Update map in contact

--- JS/contact.js
+++ JS/contact.js
@@ -1,11 +1,11 @@
 function initMap() {
-var uluru = {lat: 38.535321, lng: -121.7543463};
+var loc = {lat: 38.535321, lng: -121.7543463};
 var map = new google.maps.Map(document.getElementById('map-container'), {
   zoom: 9,
-  center: uluru
+  center: loc
 });
 var marker = new google.maps.Marker({
-  position: uluru,
+  position: loc,
   map: map
 });
 }
